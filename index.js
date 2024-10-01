fa76a783be2713ae561b4d2162caf5d9e88628b1
Malick Roungou <mjbroungou@gmail.com>
2024-10-01 06:30:44 -0400

    updates to code organization

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
 const openMenuBtn = document.getElementById('menu-btn');
 const closeMenuBtn = document.getElementById('close-btn');
-const mobileMenu = document.querySelector('#overlay')
+const mobileMenu = document.getElementById('menu-content');
+const form = document.getElementById('form');
+
+const messageData = {};
+
+const validationMessage = {
+    'empty': 'This field is required',
+    'invalid': 'Please enter a valid :attribute'
+}
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+const validateInputs = () => {
+    const inputs = document.querySelectorAll('input, textarea')
+
+    inputs.forEach(input => {
+        messageData[input.name] = input.value;
+
+        if (input.value === '') {
+            hasError(input, validationMessage.empty)
+            console.log('empty')
+        } else {
+            hasNoError(input)
+            // console.log('not empty')
+        }
+    })
+
+    const isEmail = emailRegex.test(messageData.email)
+
+    console.log(inputs);
+
+    if (!isEmail) {
+        hasError(inputs[1], validationMessage.invalid.replace(':attribute', 'email'));
+    }
+
+    console.log(messageData)
+}
+
+const hasError = (element, message) => {
+    const parentElement = element.parentElement;
+    parentElement.classList.add('has-error');
+
+    // prevents from adding multiple error divs
+    if (!parentElement.querySelector('.error-div')) {
+        const errorDiv = document.createElement('div');
+        errorDiv.textContent = message;
+        errorDiv.className = 'error-div';
+    
+        parentElement.appendChild(errorDiv);
+    }
+}
+
+const hasNoError = (element) => {
+    const parentElement = element.parentElement
+    const errorDiv = parentElement.querySelector('.error-div')
+
+    if (errorDiv) {
+        parentElement.classList.remove('has-error')
+        errorDiv.remove()
+    }
+}
+
 
 openMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.add('active');
+})
+
+closeMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('active')
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    validateInputs();
 })
