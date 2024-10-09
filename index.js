b049515bddc8a7f7c8c22070d89fff8ae8091bd5
Malick Roungou <mjbroungou@gmail.com>
2024-10-08 20:38:12 -0400

    updates

--- index.js
+++ index.js
@@ -1,30 +1,65 @@
 const openMenuBtn = document.getElementById('menu-btn');
 const closeMenuBtn = document.getElementById('close-btn');
 const mobileMenu = document.getElementById('menu-content');
+/* queries the view project btns from the portfolio page
+they will be used to dynamically put the content of the project */
+const viewProjectBtn = document.querySelectorAll('.view-project');
 const form = document.getElementById('form');
-
-const messageData = {};
-
+// when the user submits the form, their name, email and text message is saved in the obj
+// used for the validation as well
+let messageData = {};
+// the messages to be displayed when a user inputs invalid info
 const validationMessage = {
     'empty': 'This field is required',
     'invalid': 'Please enter a valid :attribute'
 }
+/* this array of objs contains the details of each project, including imgs, headers
+and description - to be dynamically added to the DOM when user clicks on the view website btn from
+the portfolio page */
+const projectsData = [
 
+    {
+        'project name': 'Manage',
+        'project summary': "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
+        'technologies': 'HTML / CSS / JS',
+        'project background': 'This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.',
+
+        'mobile': {
+            'src-1': './images/detail/mobile/image-manage-preview-1@2x.jpg',
+            'src-2': './images/detail/mobile/image-manage-preview-2@2x.jpg'
+        },
+    },
+]
+
+/* getting the html elments of h1, the hero header img, tech used, summary and the previews */
+const projectName = document.getElementById('project-name');
+const projectSummary = document.getElementById('project-summary');
+const techUsed = document.getElementById('tech');
+const projectBg = document.getElementById('project-bg');
+const previewsDiv = document.getElementById('img-previews');
+
+// regex used to test email provided
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 const validateInputs = () => {
+    // gets the name, email, and text area
     const inputs = document.querySelectorAll('input, textarea')
-
+    /* loops through the nodelist and retrieves their values and saving them into the messageData
+    object using their name as the key
+     */
     inputs.forEach(input => {
         messageData[input.name] = input.value;
-
+        // if the input is an empty string call hasError
         if (input.value === '') {
             hasError(input, validationMessage.empty)
         } else {
+            // no errors call hasNoError
             hasNoError(input)
         }
     })
-
+    /* creating an array from inputs and finding the input with the name of email
+    the email is then tested to make sure it's a valid email using the regex
+    isEmail returns a boolean */
     const emailInput = Array.from(inputs).find(input => input.name === 'email');
     const isEmail = emailRegex.test(messageData.email)
 
@@ -60,6 +95,31 @@ const hasNoError = (element) => {
     }
 }
 
+const updateProjectPage = (project) => {
+    // removing all existing text and images;
+    projectName.innerText = '';
+    projectSummary.innerText = '';
+    techUsed.innerText = '';
+    projectBg.innerText = '';
+    previewsDiv.innerHTML = '';
+
+    projectName.innerText = projectsData[project]["project name"];
+    projectSummary.innerText = projectsData[project]["project summary"]
+}
+
+viewProjectBtn?.forEach(btn => {
+    btn.addEventListener('click', () => {
+        window.location.href = './bookmark.html'
+    })
+
+    const projectIndex = Array.from(viewProjectBtn).indexOf(btn);
+    updateProjectPage(projectIndex);
+})
+
+
+window.addEventListener('DOMContentLoaded', () => {
+   updateProjectPage()
+})
 
 openMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.add('active');
